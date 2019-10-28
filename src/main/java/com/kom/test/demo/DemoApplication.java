c1a5c7f90b9ec36da09a051bdeb3ff045ed5c8cc
yuxibosashike <yuxibosashike@gmail.com>
2019-10-28 13:19:43 +0800

    添加web访问接口

--- src/main/java/com/kom/test/demo/DemoApplication.java
+++ src/main/java/com/kom/test/demo/DemoApplication.java
@@ -2,13 +2,22 @@ package com.kom.test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.print("=========>>>>>>test----------->>>>>>");
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @RequestMapping(value = "test")
+    public String getResponse(){
+        return "this is a test demo!";
+    }
+
 
 }
